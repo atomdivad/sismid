18b24f076df2a73d74b9bb2c09b424aea5efa4c4
david <davidmota@gmail.com>
2016-02-22 17:50:56 -0300

    barra do brasil

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,22 +26,23 @@
 <body>
     {{-- Top --}}
     <div class="navbar">
+        {{--<div class="row">--}}
+            {{--<div class="col-sm-12">--}}
+                {{--<!-- Brand and toggle get grouped for better mobile display -->--}}
+                {{--<div id="barra-brasil" style="background:#7F7F7F; height: 20px; display:block;">--}}
+                    {{--<ul id="menu-barra-temp" style="list-style:none;">--}}
+                        {{--<li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED">--}}
+                            {{--<a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a>--}}
+                        {{--</li>--}}
+                        {{--<li>--}}
+                            {{--<a style="font-family:sans,sans-serif; text-decoration:none; color:white;" href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a>--}}
+                        {{--</li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
         <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div id="barra-brasil" style="background:#7F7F7F; height: 20px; display:block;">
-                        <ul id="menu-barra-temp" style="list-style:none;">
-                            <li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED">
-                                <a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a>
-                            </li>
-                            <li>
-                                <a style="font-family:sans,sans-serif; text-decoration:none; color:white;" href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+
 
             <div class="row">
                 <div class="col-sm-12">
