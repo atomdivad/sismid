1d05bde8af4c174c9dba74c1993c542d28fe25ab
Vinicius Brito <viniciusbrito@users.noreply.github.com>
2016-11-05 18:20:46 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -38,7 +38,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    SisMid\Exceptions\WhoopsHandler::class,
+    //SisMid\Exceptions\WhoopsHandler::class,
     SisMid\Exceptions\Handler::class
 );
 
